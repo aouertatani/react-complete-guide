f90c7420e1af137697520c17cfce67a5e057ed11
Mohamed Ayoub OUERTATANI <ouertani.mohamed.ayoub@gmail.com>
2022-04-17 17:15:23 +0100

    36. Outputting Dynamic Data & Working with Expressions in JSX Done

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -8,10 +8,10 @@ const expenseAmount= 294.64;
 
   return (
     <div className='expense-item'>
-      <div>March 28th 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className='expense-item__description'>
         <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>$294.67</div>
+        <div className='expense-item__price'>{expenseAmount}</div>
       </div>
     </div>
   )
