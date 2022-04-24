ada564f67287066f2409bb4f1669d2e6a9e6ab54
Mohamed Ayoub OUERTATANI <ouertani.mohamed.ayoub@gmail.com>
2022-04-25 00:37:26 +0100

    59. Child-to-Parent Component Communication (Bottom-up) OK

--- src/App.js
+++ src/App.js
@@ -1,47 +1,49 @@
-import React from 'react'
-import Expenses from '../src/components/Expenses/Expenses'
-import NewExpense from './components/NewExpense/NewExpense';
+import React from 'react';
 
-const App = () =>{
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const App = () => {
   const expenses = [
     {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
-      date: new Date(2020, 8, 13)
+      date: new Date(2020, 7, 14),
     },
-    {
-      id: 'e2',
-      title: 'New TV',
-      amount: 799.94,
-      date: new Date(2021, 3, 11)
-    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
-      date: new Date(2021, 2, 28)
+      date: new Date(2021, 2, 28),
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
-      date: new Date(2021, 6, 11)
-    }
-  ]
+      date: new Date(2021, 5, 12),
+    },
+  ];
 
-  // return  React.createElement('div',
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
   //   React.createElement('h2', {}, "Let's get started!"),
   //   React.createElement(Expenses, { items: expenses })
   // );
 
   return (
     <div>
-      <NewExpense />
-        <Expenses items={expenses}></Expenses>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
-  )
-  
+  );
 }
 
-export default App
+export default App;
